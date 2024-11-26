8194abddded55b8297a02339192123b81cf50bfd
Gilles Van De Vyver <gilles.van.de.vyver@ntnu.no>
2024-11-26 11:52:51 +0100

    Improve formatting contact section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -20,68 +20,68 @@ function ContactSection() {
     });
   };
 
+  const goToLink = (url) => {
+    window.open(url, '_blank');
+  };
+
   return (
-    <div className='contact-section-container'>
-      <h2>Get in touch</h2>
-      <section className='contact-section'>
-        <div className='contact-column left-column'>
-          <p
-              className='contact-item'
-              onClick={() => copyToClipboard('gillesvandevyver@hotmail.be', 'E-mail address')}
-              onMouseEnter={() => setHoveredItem('email')}
-              onMouseLeave={() => setHoveredItem(null)}
-          >
-            <i className="fas fa-envelope"></i> gillesvandevyver@hotmail.be
-            <span className={`copy-icon ${hoveredItem === 'email' ? 'visible' : ''}`}><i
-                className="fas fa-copy"></i></span>
-          </p>
-          <p
-              className='contact-item'
-              onClick={() => copyToClipboard('+32474079443', 'Phone number')}
-              onMouseEnter={() => setHoveredItem('phone')}
-              onMouseLeave={() => setHoveredItem(null)}
-          >
-            <i className="fas fa-phone"></i> +32474079443
-            <span className={`copy-icon ${hoveredItem === 'phone' ? 'visible' : ''}`}><i
-                className="fas fa-copy"></i></span>
-          </p>
+      <div className='contact-section-container'>
+        <h2>Get in touch</h2>
+        <section className='contact-section'>
+          <div className='contact-column left-column'>
+            <p
+                className='contact-item'
+                onClick={() => copyToClipboard('gillesvandevyver@hotmail.be', 'E-mail address')}
+                onMouseEnter={() => setHoveredItem('email')}
+                onMouseLeave={() => setHoveredItem(null)}
+            >
+              <i className="fas fa-envelope"></i> gillesvandevyver@hotmail.be
+              <span className={`copy-icon ${hoveredItem === 'email' ? 'visible' : ''}`}><i
+                  className="fas fa-copy"></i></span>
+            </p>
+            <p
+                className='contact-item'
+                onClick={() => copyToClipboard('+32474079443', 'Phone number')}
+                onMouseEnter={() => setHoveredItem('phone')}
+                onMouseLeave={() => setHoveredItem(null)}
+            >
+              <i className="fas fa-phone"></i> +32474079443
+              <span className={`copy-icon ${hoveredItem === 'phone' ? 'visible' : ''}`}><i
+                  className="fas fa-copy"></i></span>
+            </p>
+          </div>
+          <div className='contact-column'>
+            <p
+                className='contact-item link-item'
+                onClick={() => goToLink('https://www.linkedin.com/in/gilles-van-de-vyver/')}
+                onMouseEnter={() => setHoveredItem('linkedin')}
+                onMouseLeave={() => setHoveredItem(null)}
+            >
+              <i className="fab fa-linkedin"></i> linkedin.com/in/gilles-van-de-vyver/
+            </p>
+            <p
+                className='contact-item link-item'
+                onClick={() => goToLink('https://scholar.google.com/citations?hl=en&user=PFAH_MMAAAAJ', 'Google Scholar profile')}
+                onMouseEnter={() => setHoveredItem('scholar')}
+                onMouseLeave={() => setHoveredItem(null)}
+            >
+              <i className="fas fa-graduation-cap"></i> Google Scholar
+            </p>
+            <p
+                className='contact-item link-item'
+                onClick={() => goToLink('https://github.com/gillesvandevyver', 'GitHub profile')}
+                onMouseEnter={() => setHoveredItem('github')}
+                onMouseLeave={() => setHoveredItem(null)}
+            >
+              <i className="fab fa-github"></i> github.com/gillesvandevyver
+            </p>
+          </div>
+        </section>
+        <div className='bottom-whitespace'>
+          <h6 className={`copied-message ${visible ? 'visible' : ''}`}>{copiedMessage}</h6>
         </div>
-        <div className='contact-column'>
-          <p
-              className='contact-item'
-              onClick={() => copyToClipboard('https://www.linkedin.com/in/gilles-van-de-vyver/', 'LinkedIn profile')}
-              onMouseEnter={() => setHoveredItem('linkedin')}
-              onMouseLeave={() => setHoveredItem(null)}
-          >
-            <i className="fab fa-linkedin"></i> linkedin.com/in/gilles-van-de-vyver/
-            <span className={`copy-icon ${hoveredItem === 'linkedin' ? 'visible' : ''}`}><i className="fas fa-copy"></i></span>
-          </p>
-          <p
-              className='contact-item'
-              onClick={() => copyToClipboard('https://scholar.google.com/citations?hl=en&user=PFAH_MMAAAAJ', 'Google Scholar profile')}
-              onMouseEnter={() => setHoveredItem('scholar')}
-              onMouseLeave={() => setHoveredItem(null)}
-          >
-            <i className="fas fa-graduation-cap"></i> Google Scholar
-            <span className={`copy-icon ${hoveredItem === 'scholar' ? 'visible' : ''}`}><i className="fas fa-copy"></i></span>
-          </p>
-          <p
-              className='contact-item'
-              onClick={() => copyToClipboard('https://github.com/gillesvandevyver', 'GitHub profile')}
-              onMouseEnter={() => setHoveredItem('github')}
-              onMouseLeave={() => setHoveredItem(null)}
-          >
-            <i className="fab fa-github"></i> github.com/gillesvandevyver
-            <span className={`copy-icon ${hoveredItem === 'github' ? 'visible' : ''}`}><i
-                className="fas fa-copy"></i></span>
-          </p>
-        </div>
-      </section>
-      <div className='bottom-whitespace'>
-        <h6 className={`copied-message ${visible ? 'visible' : ''}`}>{copiedMessage}</h6>
+        <p className='copyright'>© Copyright 2024 Gilles Van De Vyver. All rights reserved.</p>
       </div>
-      <p className='copyright'>© Copyright 2024 Gilles Van De Vyver. All rights reserved.</p>
-    </div>
   );
 }
 
